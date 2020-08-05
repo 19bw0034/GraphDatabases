9274e227e655472021ec157fd78a04ac654aff67
Beth <bethbethw@gmail.com>
2020-08-05 16:12:35 +0100

    added commands previously stored on trello

--- neo4jProject/main.py
+++ neo4jProject/main.py
@@ -1,5 +1,6 @@
 from neo4j import GraphDatabase
 
+
 class HelloWorldExample(object):
 
     def __init__(self, uri, user, password):
@@ -24,18 +25,29 @@ class HelloWorldExample(object):
         def _query_function(tx, **kwargs2):
             result = tx.run(query, **kwargs2)
             return result.data()
+
         with self._driver.session() as session:
             result = session.write_transaction(_query_function, **kwargs)
             print(result)
+            return result
 
-    @staticmethod
-    def _query_movies_by_year(tx, year):
-        result = tx.run("MATCH (a:Movie) WHERE a.released = $year RETURN a.title", year=year)
-        return result.data()
+    def query_movies_by_year(self, year):
+        return self.run_query("MATCH (a:Movie) WHERE a.released = $year RETURN a.title", year=year)
+
+    def query_all_directors(self):
+        return self.run_query("MATCH (a:Person) -[:DIRECTED]-> (Movie) WHERE (a:Person) -[:ACTED_IN]-> "
+                              "(Movie) RETURN a, COLLECT(DISTINCT Movie)")
+
+    def query_all_actors_and_directors(self):
+        return self.run("MATCH (a:Person) -[:DIRECTED]-> (c:Movie) RETURN a")
+
+    # this one doesn't work - does not have a return type
+    def delete_all_nodes_and_relationships(self):
+        return self.run("MATCH (n) DETACH DELETE n")
+
+    def find_all_actors_in_film(self, name):
+        return self.run("MATCH (a:Person) -[:ACTED_IN]-> (c:Movie) WHERE c.title = $name RETURN a", name=name)
 
 
 example = HelloWorldExample("neo4j://localhost:7687", "neo4j", "neo4j")
-example.run_query("MATCH (a:Movie) WHERE a.released = $year RETURN a.title", year=1992)
-
-
-#"MATCH (a:Movie) WHERE a.released = $year RETURN a.title", year=year
+result = example.query_movies_by_year(1992)
